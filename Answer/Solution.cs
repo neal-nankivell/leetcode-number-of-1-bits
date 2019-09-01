b31af7e41c51f89a31bdcd9a67d9875e17f022cd
neal.nankivell <neal.nankivell@gmail.com>
2019-09-01 16:37:38 +0200

    Jumped right to the bithacks O(1) solution. http://graphics.stanford.edu/~seander/bithacks.html
    I have used this in to count the number of enum flags set in the past,
    So I was fimilar with it.
    
    The simplest solution is to loop over the bits and check if each one is
    set. This is not too bad most of the time as the search space is normally
    only 32 bits.

--- Answer/Solution.cs
+++ Answer/Solution.cs
@@ -6,7 +6,11 @@ namespace Answer
     {
         public int HammingWeight(uint n)
         {
-            throw new NotImplementedException();
+            uint c = 0;
+            n = n - ((n >> 1) & 0x55555555);
+            n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+            c = ((n + (n >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
+            return unchecked((int)c);
         }
     }
 }
